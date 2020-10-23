f7b9322595a93037013d7094e7b58a2f960c92d4
TheCropp <43501093+TheCropp@users.noreply.github.com>
2020-10-23 20:57:26 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverce (n) {
+    var str = n.toString();
+   var arr = str.split("").reverse().join("");
+   var int = parseInt(arr);
+  return int;
 }
