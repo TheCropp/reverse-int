32c47b12b1aea16e7b00ef3207e9b3a995e03394
TheCropp <43501093+TheCropp@users.noreply.github.com>
2021-03-09 13:31:51 +0300

    fix: fixed reverse

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-module.exports = function reverce (n) {
+module.exports = function reverse (n) {
     var str = n.toString();
    var arr = str.split("").reverse().join("");
    var int = parseInt(arr);
